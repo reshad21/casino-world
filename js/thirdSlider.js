dab4023380ad362a2f1a05bb3318ccb000ce29dd
reshad21 <miakala7@gmail.com>
2023-02-19 09:27:36 +0600

    minimmum grid slider

---
+++ js/thirdSlider.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper3", {
+    slidesPerView: 2,
+    grid: {
+      rows: 2,
+    },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
